43ed022878051c131d1fc0d162043b3b7db99f7e
aaaayuiy <jiaxinli@divi.mn>
2016-11-14 17:05:52 -0600

    Initial Commit

---
+++ Hello-World-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello-World-Swift
+//
+//  Created by JiaxinLiDivi on 2016/11/14.
+//  Copyright © 2016年 JiaxinLi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
